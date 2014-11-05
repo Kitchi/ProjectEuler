4cf223fd3a2fa08235675a3e0e0cbac800fdc86e
Srikrishna Sekhar <kitchi.srikrishna@gmail.com>
2014-11-05 22:50:57 +0530

    011 - Largest product in a grid
    The solution for the diagonals isn't pretty, but it works!

--- 011-LargestProductInAGrid.py
+++ 011-LargestProductInAGrid.py
@@ -2,52 +2,57 @@
 
 import numpy as np
 
-grid    = np.loadtxt('011-Grid.dat')
-nrows   = ncols = 20 # from the grid 
-winSize = 4
-
 # Multiplying down and up are identical, left and right are identical.
 # So 4 functions required, horizontal, vertical, left diagonal and right diag.
 
-def maxProduct(inarr):
+def maxProduct(arr):
     """
     Calculates the product of 4 elements, using a sliding window across
     the array, and returns the maximum value of the product. 
 
     Args:
-    inarr   Input array
+    arr     Input array
 
     Returns:
     maxval  Maximum product
     """
 
-    maxprod = 1
-    thisprod = 1
-
-    arrlen = len(inarr)
-
-    if arrlen <= 4:  # List is too short, return
-        maxprod = -1
-        return maxprod
-
-    for nn in range(arrlen - 4):
-        for xx in inarr[nn:nn+4]:
-            thisprod *= xx
-
-        if thisprod > maxprod:
-            maxprod = thisprod
-        
-        thisprod = 1
-
+    maxprod = np.amax([np.prod(arr[x:x+4]) for x in range(len(arr))])
     return maxprod
 
-def getDiagonalElements(matrix):
+def diagProd(gridn):
     """
-    Returns a list of lists; Each (sub)list will contain 
+    Get all diagonals in direction of the main diagonal, and return max product
+    among every 4 elements.
+
+    Args:
+    grid    The input 2D grid
+
+    Returns:
+    maxprod THe max. product among every set of 4 elements.
     """
 
-colprods = [maxProduct(col) for col in grid.T]
-maxcolprod = max(colprods)
+    maxprod = 0
+    for rr, row in enumerate(gridn):
+        for cc, col in enumerate(row):
+            try:
+                prod = gridn[rr, cc] * gridn[rr+1, cc+1] * gridn[rr+2, cc+2] * gridn[rr+3, cc+3]
+                if prod > maxprod:
+                    maxprod = prod
+            except IndexError:
+                pass
+    return (maxprod)
 
-rowprods = [maxProduct(row) for row in grid]
-maxrowprod = max(rowprods)
+
+grid    = np.loadtxt('011-Grid.dat')
+nrows   = ncols = 20 # from the grid 
+winSize = 4
+
+maxcol  = np.amax([maxProduct(col) for col in grid.T])
+maxrow  = np.amax([maxProduct(row) for row in grid])
+
+maxd2   = diagProd(np.rot90(grid))
+maxd1   = diagProd(grid)
+
+maxprod = np.amax([maxcol, maxrow, maxd1, maxd2])
+print(maxprod)
